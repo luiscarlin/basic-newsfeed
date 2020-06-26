aeaafd76bd3194a592c44bec86186cdaa79a5a45
Luis Carlin <luiscarlin@gmail.com>
2020-06-25 22:36:24 -0400

    setup react + parcel + typescript

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Main = styled.main`
+  background: red;
+`;
+
+export const App = () => {
+  return <Main>Hello world</Main>;
+};
