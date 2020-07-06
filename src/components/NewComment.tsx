0a19eb6057acb5b24b9a884a115a1fb6a880c293
Luis Carlin <luiscarlin@gmail.com>
2020-07-06 02:13:03 -0400

    style new comment box

--- src/components/NewComment.tsx
+++ src/components/NewComment.tsx
@@ -1,9 +1,33 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import { SmallProfilePic } from '../styles/CardStyles';
+import { colors } from '../styles/colors';
 
 const ProfilePic = styled(SmallProfilePic)`
-  margin: 2rem;
+  margin-right: 2rem;
+`;
+
+const InputBox = styled.input`
+  width: 100%;
+  height: 100%;
+  border: 2px solid ${colors.border};
+  font-size: 1.5rem;
+  color: ${colors.text};
+  font-weight: 500;
+  line-height: 1.5;
+  padding: 1.5rem;
+  background: transparent;
+  border-radius: 50px;
+  ::placeholder {
+    color: ${colors.placeholderText};
+  }
+`;
+
+const NewCommentWrapper = styled.div`
+  display: flex;
+  padding: 1rem 2rem;
+  align-items: center;
+  background: ${colors.actionBarBackground};
 `;
 
 export interface NewCommentStateProps {
@@ -26,9 +50,14 @@ export const NewComment = ({ photoUrl, onNewComment }: NewCommentProps) => {
   };
 
   return (
-    <div>
+    <NewCommentWrapper>
       <ProfilePic src={photoUrl} alt="profile-pic" />
-      <input value={comment} onChange={(e) => setComment(e.target.value)} onKeyDown={handleKeyDown} />
-    </div>
+      <InputBox
+        placeholder={'Add a comment'}
+        value={comment}
+        onChange={(e) => setComment(e.target.value)}
+        onKeyDown={handleKeyDown}
+      />
+    </NewCommentWrapper>
   );
 };
